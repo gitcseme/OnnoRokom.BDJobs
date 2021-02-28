e0877b61108df8f6fb0b7ff1004c7af8b8d2ee72
kashuvo <shuvo_CSE23@yahoo.com>
2021-02-28 19:16:21 +0600

    job-service worked & autofac is working

--- OnnoRokom.BDJobs.Web/Controllers/HomeController.cs
+++ OnnoRokom.BDJobs.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnnoRokom.BDJobs.JobsLib.Entities;
+using OnnoRokom.BDJobs.JobsLib.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +10,24 @@ namespace OnnoRokom.BDJobs.Web.Controllers
 {
     public class HomeController : Controller
     {
+        public IJobService _jobService { get; }
+
+        public HomeController(IJobService jobService)
+        {
+            _jobService = jobService;
+        }
+
         public ActionResult Index()
         {
+            var job = new Job
+            {
+                Title = "Test Job",
+                Description = "This is testing job post",
+                EmployerId = new Guid("b1d9a435-688b-49ea-a555-56013ef065fd")
+            };
+
+            _jobService.Create(job);
+
             return View();
         }
 
