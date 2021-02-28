4ca30cd29fb3db575a0d9d0531763aa8bcd75d4e
kashuvo <shuvo_CSE23@yahoo.com>
2021-02-28 13:02:55 +0600

    basic entity & relationships are designed

---
+++ Libraries/OnnoRokom.BDJobs.JobsLib/Mappings/JobMap.cs
@@ -0,0 +1,27 @@
+﻿using FluentNHibernate.Mapping;
+using OnnoRokom.BDJobs.JobsLib.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnnoRokom.BDJobs.JobsLib.Mappings
+{
+    public class JobMap : ClassMap<Job>
+    {
+        public JobMap()
+        {
+            Table("Jobs");
+
+            Id(j => j.Id).GeneratedBy.Guid();
+            Map(j => j.EmployerId).Not.Nullable();
+            Map(j => j.Title).Length(70).Not.Nullable();
+            Map(j => j.Description).Length(1000).Not.Nullable();
+
+            HasManyToMany(j => j.Candidates)
+                .Cascade.All()
+                .Table("JobCandidate");
+        }
+    }
+}
