29820739e843d4269c8dd755dc3a1e658a4b5def
kashuvo <shuvo_CSE23@yahoo.com>
2021-03-02 19:38:32 +0600

    Admin panel is created

---
+++ OnnoRokom.BDJobs.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,36 @@
+﻿using OnnoRokom.BDJobs.JobsLib.Services;
+using OnnoRokom.BDJobs.Web.Areas.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnnoRokom.BDJobs.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class DashboardController : Controller
+    {
+        private IJobService _jobService { get; }
+        private readonly ApplicationUserManager _userManager;
+
+        public DashboardController(IJobService jobService, ApplicationUserManager userManager = null)
+        {
+            _jobService = jobService;
+            _userManager = userManager;
+        }
+
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+
+            var model = new AdminDashboardViewModel
+            {
+                TotalUsers = _userManager.Users.Count(),
+                TotalJobPosts = _jobService.GetAll().Count()
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ OnnoRokom.BDJobs.Web/Areas/Admin/Models/AdminDashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnnoRokom.BDJobs.Web.Areas.Admin.Models
+{
+    public class AdminDashboardViewModel
+    {
+        public int TotalUsers { get; set; }
+        public int TotalJobPosts { get; set; }
+
+    }
+}
